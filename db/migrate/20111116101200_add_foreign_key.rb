5bc9fe760b4cbdd9dd02289d3d30f0049419bb4d
Eric Rochester <err8n@virginia.edu>
2011-11-16 10:25:37 -0500

    Fixed pluralization problems.

--- db/migrate/20111116101200_add_foreign_key.rb
+++ db/migrate/20111116101200_add_foreign_key.rb
@@ -5,22 +5,22 @@ class AddForeignKey < ActiveRecord::Migration
     add_column :patrons, :events_id, :integer
     add_column :military_orders, :events_id, :integer
     add_column :building_types, :events_id, :integer
-    add_column :monastic_metadatum, :events_id, :integer
-    add_column :fortress_metadatum, :events_id, :integer
-    add_column :church_metadatum, :events_id, :integer
+    add_column :monastic_metadata, :events_id, :integer
+    add_column :fortress_metadata, :events_id, :integer
+    add_column :church_metadata, :events_id, :integer
     add_column :site_principalities, :sites_id, :integer
     add_column :building_topographies, :buildings_id, :integer
     add_column :event_types, :events_id, :integer
     add_column :patron_names, :patrons_id, :integer
     add_column :patron_types, :patrons_id, :integer
-    add_column :patron_kingdom, :patrons_id, :integer
+    add_column :patron_kingdoms, :patrons_id, :integer
     add_column :patron_dates, :patrons_id, :integer
     add_column :order_names, :orders_id, :integer
-    add_column :fortmeta_settlements, :fortress_metadatum_id, :integer
-    add_column :fortmeta_plantypes, :fortress_metadatum_id, :integer
-    add_column :fortmeta_monast_blgs :fortress_metadatum_id, :integer
-    add_column :churchmeta_types, :church_metadatum_id, :integer
-    add_column :monastmeta_bldgs, :monastic_metadatum, :integer
+    add_column :fortmeta_settlements, :fortress_metadata_id, :integer
+    add_column :fortmeta_plantypes, :fortress_metadata_id, :integer
+    add_column :fortmeta_monast_blgs, :fortress_metadata_id, :integer
+    add_column :churchmeta_types, :church_metadata_id, :integer
+    add_column :monastmeta_bldgs, :monastic_metadata, :integer
     add_column :milorders_names, :military_order, :integer
   end
 end
