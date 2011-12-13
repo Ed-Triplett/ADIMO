ba59f29eaaa25cfeaa486744897b1376464a58c6
Ed Triplett <edward.triplett@gmail.com>
2011-12-13 10:18:44 -0500

    Commented out change to sites model 'through' relationship

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,7 +2,7 @@ class Site < ActiveRecord::Base
   has_many :buildings
   has_many :events, :through => :buildings
   has_many :military_orders, :through => :events
-  has_many :building_types, :through => :buildings
+  #has_many :building_types, :through => :buildings
   
   belongs_to :site_principality
   
