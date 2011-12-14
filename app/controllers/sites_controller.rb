a6c2aa110728856e8377f59403c5501a054839c6
Ed Triplett <edward.triplett@gmail.com>
2011-12-14 11:04:04 -0500

    Changes to the site model and site Controller to get different fields to appear at the top of the hierarchy.

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -9,14 +9,14 @@ class SitesController < ApplicationController
     # conf.columns = [:name, :principality, :buildings, :military_order, :patrons, :notes]
     conf.create.columns = [:name, :site_principality, :notes]
     conf.update.columns = [:name, :site_principality, :notes]
-    conf.show.columns = [:name, :site_principality, :buildings, :military_orders, :patrons, :events, :notes]
+    conf.show.columns = [:name, :site_principality, :buildings, :military_orders, :patrons, :event_types :notes]
     conf.columns[:name].label = "Name"
     conf.columns[:site_principality].label = "Principality"
     conf.columns[:site_principality].form_ui = :select
     
     conf.columns[:patrons].actions_for_association_links = [:show]
     conf.columns[:military_orders].actions_for_association_links = [:show]
-    conf.columns[:events].actions_for_association_links = [:show]
+    conf.columns[:event_types].actions_for_association_links = [:show]
     conf.columns[:site_principality].actions_for_association_links = [:show]
     
     list.columns.exclude :notes
--- app/models/site.rb
+++ app/models/site.rb
@@ -1,8 +1,9 @@
 class Site < ActiveRecord::Base
   has_many :buildings
-  has_many :events, :through => :buildings
+  has_many :event_types, :through => :events
   has_many :military_orders, :through => :events
   has_many :patrons, :through => :events
+  has_many :building_type, :through => :buildings
   
   
   belongs_to :site_principality
