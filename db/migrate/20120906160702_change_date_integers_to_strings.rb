d37ba74c3e33a51c83a9ef3a756b64e41964fb79
Ed Triplett <edward.triplett@gmail.com>
2012-09-10 18:48:56 -0400

    Turned off Devise config.use_salt_as_remeber_token = true

--- db/migrate/20120906160702_change_date_integers_to_strings.rb
+++ db/migrate/20120906160702_change_date_integers_to_strings.rb
@@ -17,35 +17,3 @@ class ChangeDateIntegersToStrings < ActiveRecord::Migration
   	change_column :events, :latest_end_date, :integer
   end
 end
-
-#class ChangeDateIntegersToStrings < ActiveRecord::Migration
-
-  #def self.up
-
-    #change_table :events do |t|
-
-      #t.change :begin_date, :integer
-      #t.change :earliest_begin_date, :integer
-      #t.change :latest_begin_date, :integer
-      #t.change :end_date, :integer
-      #t.change :earliest_end_date, :integer
-      #t.change :latest_end_date, :integer
-
-    #end
-  #end
-
-  #def self.down
-
-    #change_table :events do |t|
-
-      #t.change :begin_date, :string
-      #t.change :earliest_begin_date, :string
-      #t.change :latest_begin_date, :string
-      #t.change :end_date, :string
-      #t.change :earliest_end_date, :string
-      #t.change :latest_end_date, :string
-
-    #end
-  #end
-
-#end
